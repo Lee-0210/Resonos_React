e846a2c39e7cb02608153ee16e0e4a76f09d617b
skymin022 <wlqtk44@gmail.com>
2025-08-18 16:02:08 +0900

    커뮤 - 커뮤이름, 데이터 오류 수정 완

--- resonos/src/main/java/com/cosmus/resonos/mapper/community/BoardPostMapper.java
+++ resonos/src/main/java/com/cosmus/resonos/mapper/community/BoardPostMapper.java
@@ -27,7 +27,6 @@ public interface BoardPostMapper {
     public int countByUserId(Long userId);
     // 조회 + 좋아요 싫어요 수
     public BoardPost selectWithLikesDislikes(Long id) throws Exception;
-
     // 커뮤 main
     // 주요뉴스 (가장 화제글 3개 + 썸네일) - 예시: 조회수 기준
     public List<BoardPost> selectHotPosts(int limit);
