15eb271f9dbc52ded6b91b821e2316f2032ef2bd
Lee-0210 <tjsans9069@gmail.com>
2025-08-19 18:00:57 +0900

    나는 리액트에 뇌가 절여졌다

--- resonos-app/src/components/community/card/BoardResultCard.jsx
+++ resonos-app/src/components/community/card/BoardResultCard.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
-const BoardResultCard = ({rank}) => {
+const BoardResultCard = ({rank, board}) => {
 
   const navigate = useNavigate()
 
   const handleNavigate = () => {
-    navigate('/community/boards/98')
+    navigate(`/community/boards/${board.id}`)
   }
 
   return (
     <li onClick={handleNavigate}>
-      <span className='board-title focus'>게시판 이름</span>
-      <span>{rank}위</span>
+      <span className='board-title focus'>{board?.name}</span>
+      {/* <span>{rank}위</span> */}
       <div>
         <span>게시글</span>
         <span className='date'>85,300</span>
